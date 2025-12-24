7b405c16631f422f88336b085f3fe4d2fb4a1475
e7df333d78k8zfd-arch <e7df333d78k8zfd@gmail.com>
2025-12-24 18:36:08 +0900

    Update service worker cache to version 2

--- sw.js
+++ sw.js
@@ -1,7 +1,12 @@
 self.addEventListener("install", e => {
   e.waitUntil(
-    caches.open("memo-cache").then(cache =>
-      cache.addAll(["index.html"])
+    caches.open("memo-cache-v2").then(cache =>
+      cache.addAll([
+        "index.html",
+        "manifest.json",
+        "icon-192.png",
+        "icon-512.png"
+      ])
     )
   );
 });
