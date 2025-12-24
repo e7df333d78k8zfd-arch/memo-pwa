cd2ad4fec540cac2c34cceb22231371670495eae
e7df333d78k8zfd-arch <e7df333d78k8zfd@gmail.com>
2025-12-24 18:48:23 +0900

    Update service worker cache version to v3

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.addEventListener("install", e => {
   e.waitUntil(
-    caches.open("memo-cache-v2").then(cache =>
+    caches.open("memo-cache-v3").then(cache =>
       cache.addAll([
         "index.html",
         "manifest.json",
